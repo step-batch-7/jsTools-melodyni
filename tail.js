c1bb57a67381255435f41d5c25de46609acb729d
RaginiKumari <58027796+melodyni@users.noreply.github.com>
2019-12-23 12:19:45 +0530

    added try catch to handle badFile in commandadded try catch to handle badFile in commandadded try catch to handle badFile in commandadded try catch to handle badFile in commandadded try catch to handle badFile in commandadded try catch to handle badFile in commandadded try catch to handle badFile in commandadded try catch to handle badFile in command

--- tail.js
+++ tail.js
@@ -1,8 +1,4 @@
 "use strict";
-process.on("uncaughtException", err => {
-  console.error(err.message);
-  process.exit(0);
-});
 
 const { parseUserArgs, selectLastN, loadFile } = require("./src/tailLib");
 const { getFileAction } = require("./src/fileUtil");
@@ -10,9 +6,13 @@ const { getFileAction } = require("./src/fileUtil");
 const main = function() {
   const parsedArgs = parseUserArgs(process.argv.slice(2));
   const fileAction = getFileAction(parsedArgs.filePath);
-  const content = loadFile(fileAction);
-  const tail = selectLastN(content, parsedArgs.tailLength);
-  console.log(tail.join("\n"));
+  try {
+    const content = loadFile(fileAction);
+    const tail = selectLastN(content, parsedArgs.tailLength);
+    console.log(tail.join("\n"));
+  } catch (error) {
+    console.error(error.message);
+  }
 };
 
 main();
